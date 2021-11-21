104544e9159401954bed0aa898404c68e5609fdc
Dylan Honeyman <dylan@dylans-iMac.local>
2021-11-21 12:42:18 -0600

    date converter works

--- Assets/script.js
+++ Assets/script.js
@@ -27,10 +27,6 @@ function fiveDay() {
         let lat = fiveDay.city.coord.lat;
         let lon = fiveDay.city.coord.lon;
         
-        
-    
-        
-        
         searchedCity = document.querySelector('#searched-city').value;
         var requestUrl2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=6a86bc2b6b72e3f1aa5395dbb3a0c828
         `;
@@ -45,17 +41,31 @@ function fiveDay() {
             // changing date
             $('.five-card .card-title').each(function(){
                 let dayNumber = $(this).attr('data-number');
-                $('.card-title').text() = dataset2.daily[dayNumber].dt;
-                $('.temp').text() = dataset2.daily[dayNumber].temp.day;
-                $('.wind').text() = dataset2.daily[dayNumber].wind_speed;
-                $('.humid').text() = dataset2.daily[dayNumber].humidity;
+                let dataDate = dataset2.daily[dayNumber].dt;
+                // $('.card-title').text() = dataset2.daily[dayNumber].dt;
+                // $('.temp').text() = dataset2.daily[dayNumber].temp.day;
+                // $('.wind').text() = dataset2.daily[dayNumber].wind_speed;
+                // $('.humid').text() = dataset2.daily[dayNumber].humidity;
                 // $('.card-title').text() = 'test'
+                console.log(dataDate);
+
+                console.log(dateConverter(dataDate));
             })
             
         
     })
     })
     }
+
+    function dateConverter(dt){
+        let date = new Date(dt*1000);
+        let month = date.getMonth() + 1;
+        let day = date.getDate();
+        let year = date.getFullYear();
+        return `${month}/${day}/${year}`;
+    }
+
+    function 
 
 
 cityBtn.addEventListener('click',fiveDay);
